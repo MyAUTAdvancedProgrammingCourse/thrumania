750af3ad522da07835f392a2c5ad0a1b83c6ac4a
mahdigolmohamadi96 <m_mahdi_g96@yahoo.com>
2016-06-25 13:10:08 +0430

    startup for  shios

--- src/com/poorgroupproject/thrumania/item/vehicle/FishingShip.java
+++ src/com/poorgroupproject/thrumania/item/vehicle/FishingShip.java
@@ -1,7 +1,43 @@
 package com.poorgroupproject.thrumania.item.vehicle;
 
+import com.poorgroupproject.thrumania.events.Event;
+import com.poorgroupproject.thrumania.item.GameObject;
+import com.poorgroupproject.thrumania.item.place.Port;
+
 /**
- * Created by ahmad on 5/31/16.
+ *@author yahya
+ * @version 1.0.0
  */
-public class FishingShip {
+public class FishingShip extends Ship {
+
+    Port port;
+    int peopleNum;
+    final int peopleCapacity=100;
+    int foodNum=0;
+
+    int shipCapacity;
+    public FishingShip(Port port ,int x, int y, int width, int height) {
+        super(x, y, width, height);
+        this.port=port;
+    }
+
+    public void IsEmpty(){
+       shipCapacity= peopleNum *peopleCapacity;
+        if(foodNum==shipCapacity){
+            //send event to people stop fishing
+            //chang the image
+
+
+        }
+    }
+
+    @Override
+    public void loadResoure() {
+
+    }
+
+    @Override
+    public void processEvent(Event event) {
+
+    }
 }
