56f2458bf95552c4f40b4d88bdde4445bf372fc0
mahdigolmohamadi96 <m_mahdi_g96@yahoo.com>
2016-06-25 17:01:21 +0430

    adding aStop fishing event

--- src/com/poorgroupproject/thrumania/item/vehicle/FishingShip.java
+++ src/com/poorgroupproject/thrumania/item/vehicle/FishingShip.java
@@ -1,8 +1,14 @@
 package com.poorgroupproject.thrumania.item.vehicle;
 
+import com.poorgroupproject.thrumania.events.ClickEvent;
 import com.poorgroupproject.thrumania.events.Event;
+import com.poorgroupproject.thrumania.events.StopFishingEvent;
 import com.poorgroupproject.thrumania.item.GameObject;
+import com.poorgroupproject.thrumania.item.human.Citizen;
+import com.poorgroupproject.thrumania.item.human.Human;
 import com.poorgroupproject.thrumania.item.place.Port;
+
+import java.util.ArrayList;
 
 /**
  *@author yahya
@@ -12,13 +18,26 @@ public class FishingShip extends Ship {
 
     Port port;
     int peopleNum;
-    final int peopleCapacity=100;
+    final int peopleCapacity=1200;
     int foodNum=0;
 
     int shipCapacity;
+    Human []fisherMan;
+
+
+
     public FishingShip(Port port ,int x, int y, int width, int height) {
         super(x, y, width, height);
         this.port=port;
+        fisherMan =new Human[7];
+    }
+
+
+
+    public void getHumqn(){
+        for(int i=0;i<7;i++){
+           // fisherMan[i]=pickUpHuman;
+        }
     }
 
     public void IsEmpty(){
@@ -26,6 +45,8 @@ public class FishingShip extends Ship {
         if(foodNum==shipCapacity){
             //send event to people stop fishing
             //chang the image
+            Human h =new Citizen(0,0);
+            h.processEvent(new StopFishingEvent(this));
 
 
         }
@@ -39,5 +60,10 @@ public class FishingShip extends Ship {
     @Override
     public void processEvent(Event event) {
 
+
+    }
+
+    public void setPort(Port port) {
+        this.port = port;
     }
 }
