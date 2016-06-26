d0721ff666b1ebf4ee26365358da4efb318aeaa0
mahdigolmohamadi96 <m_mahdi_g96@yahoo.com>
2016-06-26 14:05:11 +0430

    adding aStop fishing event

--- src/com/poorgroupproject/thrumania/item/vehicle/TransportShip.java
+++ src/com/poorgroupproject/thrumania/item/vehicle/TransportShip.java
@@ -1,7 +1,19 @@
 package com.poorgroupproject.thrumania.item.vehicle;
 
 /**
- * Created by ahmad on 5/31/16.
+ * @author yahay
+ * @version 1.0.0
  */
-public class TransportShip  {
+public class TransportShip  extends Ship implements Runnable{
+    public TransportShip(int x, int y) {
+        super(x, y, 256, 256);
+    }
+
+    @Override
+    public void run() {
+        for(int i=0;i<20;i++){
+            setX(getX()+1);
+            setY(getY()+1);
+        }
+    }
 }
