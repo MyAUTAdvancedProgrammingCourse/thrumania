6f133429585f89b09d46ec4fa17b47acbc2f6785
anvari1313 <ahmad.anv@gmail.com>
2016-06-26 17:53:44 +0430

    makeup the main method again

--- src/com/poorgroupproject/thrumania/Thrumania.java
+++ src/com/poorgroupproject/thrumania/Thrumania.java
@@ -3,6 +3,7 @@ package com.poorgroupproject.thrumania;
 import com.poorgroupproject.thrumania.backgroundprocess.Season;
 import com.poorgroupproject.thrumania.form.*;
 import com.poorgroupproject.thrumania.form.Frame;
+import com.poorgroupproject.thrumania.land.Land;
 import com.poorgroupproject.thrumania.panel.GamePanel;
 import com.poorgroupproject.thrumania.util.GameConfig;
 import com.poorgroupproject.thrumania.util.ScreenRepainter;
@@ -12,24 +13,36 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 /**
  * @author ahmad
  * @version 1.0.0
  */
 public class Thrumania {
-//    public static void main(String[] args) {
-//        EventQueue.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                Frame f = new Frame();
-//                GamePanel g = new GamePanel(f.getWidth(),f.getHeight());
-//                ScreenRepainter repainter = new ScreenRepainter(g);
-//                f.add(g);
-//                f.showFrame();
-//
-//            }
-//        });
+    public static void main(String[] args) {
+        GameConfig.setFrameDelayTime();
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Frame f = new Frame();
+                GamePanel g = new GamePanel(f.getWidth(),f.getHeight());
+                ScreenRepainter repainter = new ScreenRepainter(g);
+                repainter.start();
+                f.add(g);
+                f.showFrame();
+                Thread screenRepainter = new ScreenRepainter(g);
+                screenRepainter.start();
+            }
+        });
 
-//    }
+
+        try {
+            Land.getInstance().loadMap(new File("resource/map/map1.tmf"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
