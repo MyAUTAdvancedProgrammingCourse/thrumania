30ade00005ac07e3d14185fed42212f496322405
anvari1313 <ahmad.anv@gmail.com>
2016-06-27 22:49:15 +0430

    add setter and getter method for the location of the PlayerPanel

--- src/com/poorgroupproject/thrumania/panel/PlayerPanel.java
+++ src/com/poorgroupproject/thrumania/panel/PlayerPanel.java
@@ -15,15 +15,30 @@ import java.io.IOException;
 public class PlayerPanel{
     private BufferedImage view;
     private Rectangle boundry;
+    private Image player;
+    private Dimension playerFaceDimension;
+
     public PlayerPanel(){
         boundry = new Rectangle();
         boundry.setLocation(0,0);
-        boundry.setSize(100,100);
+        boundry.setSize(200,400);
+        playerFaceDimension = new Dimension(50,50);
         try {
             view = ImageIO.read(new File("resource/image/item/spring.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Graphics graphics = view.getGraphics();
+        graphics.setColor(Color.gray);
+        graphics.setFont(new Font("Arial Black", Font.BOLD, 140));
+        graphics.drawString("some text", 110, ((int) (.5 * boundry.getHeight())));
+        try {
+            player = ImageIO.read(new File("resource/image/player/face.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        graphics.drawImage(player,(((int) (boundry.getWidth() - playerFaceDimension.getWidth()))) / 2, ((int) (.3 * boundry.getHeight()))
+                , ((int) playerFaceDimension.getWidth()), ((int) playerFaceDimension.getHeight()),null);
     }
 
     public Rectangle getBoundry(){
@@ -31,5 +46,11 @@ public class PlayerPanel{
     }
     public BufferedImage getView(){
         return view;
+    }
+    public void setLocation(Point location){
+        boundry.setLocation(location);
+    }
+    public Point getLocation(){
+        return boundry.getLocation();
     }
 }
