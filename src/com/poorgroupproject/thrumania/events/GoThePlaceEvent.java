7bac8432527c7a232b253d0589fd175a91465d9b
SamanMirhosseini <samanmirhoseini@aut.ac.ir>
2016-06-30 16:50:15 +0430

    Human movement

--- src/com/poorgroupproject/thrumania/events/GoThePlaceEvent.java
+++ src/com/poorgroupproject/thrumania/events/GoThePlaceEvent.java
@@ -1,6 +1,8 @@
 package com.poorgroupproject.thrumania.events;
 
 import com.poorgroupproject.thrumania.item.GameObject;
+import com.poorgroupproject.thrumania.pathfinder.Pair;
+import com.poorgroupproject.thrumania.pathfinder.Path;
 
 /**
  * Created by Saman A.Mirhoseini on 29/06/2016.
@@ -10,5 +12,12 @@ public class GoThePlaceEvent extends Event {
     public int targetY;
     public GoThePlaceEvent(GameObject sender,int x,int y) {
         super(sender);
+        targetX = x;
+        targetY = y;
+ = }
+    public GoThePlaceEvent(GameObject sender, Pair p) {
+        super(sender);
+        targetX = p.getX();
+        targetY = p.getY();
     }
 }
