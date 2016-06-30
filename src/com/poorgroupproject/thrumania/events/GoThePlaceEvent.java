9504ecef5e33be79d53646b835d6a9b25f1b3734
anvari1313 <ahmad.anv@gmail.com>
2016-06-30 12:24:34 +0430

    new constructor

--- src/com/poorgroupproject/thrumania/events/GoThePlaceEvent.java
+++ src/com/poorgroupproject/thrumania/events/GoThePlaceEvent.java
@@ -1,6 +1,7 @@
 package com.poorgroupproject.thrumania.events;
 
 import com.poorgroupproject.thrumania.item.GameObject;
+import com.poorgroupproject.thrumania.pathfinder.Pair;
 
 /**
  * Created by Saman A.Mirhoseini on 29/06/2016.
@@ -10,5 +11,11 @@ public class GoThePlaceEvent extends Event {
     public int targetY;
     public GoThePlaceEvent(GameObject sender,int x,int y) {
         super(sender);
+    }
+
+    public GoThePlaceEvent(GameObject sender, Pair pair){
+        super(sender);
+        targetX = pair.getX();
+        targetY = pair.getY();
     }
 }
