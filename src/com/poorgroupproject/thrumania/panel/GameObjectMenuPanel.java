55c0b1671069465c2c246a8e434550b6cbaac1e4
anvari1313 <ahmad.anv@gmail.com>
2016-07-03 08:57:03 +0430

    add GameObjectPanel

--- src/com/poorgroupproject/thrumania/panel/GameObjectMenuPanel.java
+++ src/com/poorgroupproject/thrumania/panel/GameObjectMenuPanel.java
@@ -1,7 +1,33 @@
 package com.poorgroupproject.thrumania.panel;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
- * Created by Ahmad on 7/2/2016.
+ * @author ahmad
+ * @version 1.0.0
  */
 public class GameObjectMenuPanel {
+    private BufferedImage view;
+    private ArrayList<String> choices;
+    private final int PANEL_WIDTH = 100;
+    private final int PANEL_CHOICE_HEIGHT = 30;
+    private Rectangle boundry;
+
+
+    public GameObjectMenuPanel(ArrayList<String> choices){
+        this.choices = choices;
+        boundry = new Rectangle(0,0,PANEL_WIDTH,choices.size() * PANEL_CHOICE_HEIGHT);
+        view = new BufferedImage(((int) boundry.getWidth()), ((int) boundry.getHeight()),BufferedImage.TYPE_INT_ARGB);
+        drawItems(view.getGraphics());
+    }
+
+    private void drawItems(Graphics graphics){
+        for (int i = 0; i < choices.size(); i++) {
+
+        }
+
+        }
+    }
 }
