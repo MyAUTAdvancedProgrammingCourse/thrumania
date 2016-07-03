27419c508d286e729b5013b59516cf2220e65c98
Amin <a.rashidbeigi@gmail.com>
2016-07-03 14:06:48 +0430

    some changes in Palace class

--- src/com/poorgroupproject/thrumania/item/place/Palace.java
+++ src/com/poorgroupproject/thrumania/item/place/Palace.java
@@ -39,20 +39,6 @@ public class Palace extends Place{
         for (int i = 0; i < 10; i++){
             //10 nafar avalie bayad dashte bashad
         }
-
-        (new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    setX(getX() + 2);
-                    try {
-                        Thread.sleep(40);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        })).start();
     }
 
 
@@ -63,7 +49,7 @@ public class Palace extends Place{
     public void loadResoure() {
         palaceImages = new Image[3];
         try {
-            palaceImages[0] = ImageIO.read(new File(ResourcePath.itemImagePath + "palace/palace_3.png"));
+            palaceImages[2] = ImageIO.read(new File(ResourcePath.itemImagePath + "palace/palace_3.png"));
         } catch (IOException e) {
             System.err.println("FILE NOT FOUND");
             e.printStackTrace();
